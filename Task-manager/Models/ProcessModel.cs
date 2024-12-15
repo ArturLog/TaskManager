1abab73cefc670f0d931d3731ae2f1a530e4ab8b
ArturLog <artur.spiewak2967@gmail.com>
2024-12-16 00:47:44 +0100

    Init models

---
+++ Task-manager/Models/ProcessModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Task_manager.Models
+{
+    public class ProcessModel
+    {
+        public int ProcessId { get; set; }
+        public string Name { get; set; }
+        public double MemoryUsage { get; set; }
+        public string Status { get; set; }
+        public List<int> Threads { get; set; }
+        public List<string> Modules { get; set; }
+    }
+}
---
+++ Task-manager/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_manager.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public DateTime ExecutionTime { get; set; }
+        public string Command { get; set; }
+        public int ExecutionCount { get; set; }
+        public TimeSpan TimeToNextExecution { get; set; }
+    }
+}
