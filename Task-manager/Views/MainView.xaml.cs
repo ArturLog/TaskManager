daa7f1c373aa4a1dfc6700a208c863fc24d6af41
ArturLog <artur.spiewak2967@gmail.com>
2024-12-16 00:48:05 +0100

    Create templates

---
+++ Task-manager/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using Task_manager.ViewsModels.Classes;
+
+namespace Task_manager.Views
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
