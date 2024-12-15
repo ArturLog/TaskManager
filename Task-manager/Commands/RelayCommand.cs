3f8f19b96abfe6e4e6a417a6b4a77f5c91d32672
ArturLog <artur.spiewak2967@gmail.com>
2024-12-16 00:47:31 +0100

    Add can execute

---
+++ Task-manager/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.CodeDom.Compiler;
+using System.Windows.Input;
+
+namespace Task_manager.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+            : this(
+                param => execute(),
+                canExecute != null ? new Func<object?, bool>(_ => canExecute()) : null
+            )
+        {
+        }
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
